a9592c09037ac701f45f5395e37403e52fcae876
Jorge Andres Layos Aguirre <layos91@gmail.com>
2023-04-29 08:46:58 -0500

    finished ticket validation

--- parcial.Shared/Entities/Ticket.cs
+++ parcial.Shared/Entities/Ticket.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace parcial.Shared.Entities
+﻿namespace parcial.Shared.Entities
 {
     public class Ticket
     {
