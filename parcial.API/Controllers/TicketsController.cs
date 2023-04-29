ebb2df4beea4c8aca505d247fcc7ed5cc6c2c2ec
Jorge Andres Layos Aguirre <layos91@gmail.com>
2023-04-29 01:17:10 -0500

    index 2.0

--- parcial.API/Controllers/TicketsController.cs
+++ parcial.API/Controllers/TicketsController.cs
@@ -21,7 +21,45 @@ namespace parcial.API.Controllers
         {
             return Ok(await _context.Tickets.ToListAsync());
         }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var ticket = await _context.Tickets.FirstOrDefaultAsync(x => x.Id == id);
+            if (ticket is null)
+            {
+                return NotFound();
+            }
 
+            return Ok(ticket);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Ticket ticket)
+        {
+            _context.Update(ticket);
+            try
+            {
+
+                await _context.SaveChangesAsync();
+                return Ok(ticket);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe un registro con el mismo nombre.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+
+        }
         [HttpPost]
         public async Task<ActionResult> Post(Ticket ticket)
         {
